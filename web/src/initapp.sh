6028cdf7293f662517f91ed812e139f27a942c11
sergey243 <sergeyassu@gmail.com>
2024-04-03 11:34:48 +0100

    super user creqtion automation

--- web/src/initapp.sh
+++ web/src/initapp.sh
@@ -1,7 +1,5 @@
 echo "Migrating"
 python manage.py migrate
-echo "Create super user"
-python manage.py createsuperuser
 echo "Loading fixtures"
 python manage.py loaddata ./basedata.json --exclude auth.permission
 echo "Translation"

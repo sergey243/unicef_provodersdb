5e186a25ed409e5a1976e67f07181129e4de7f80
sergey243 <sergeyassu@gmail.com>
2024-04-03 16:08:18 +0100

    docker compose update

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -5,6 +5,10 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 # Apply database migrations
+echo "Apply mqking migrations"
+python manage.py makemigrations
+
+# Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
 
